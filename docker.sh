9df3ca7529b667c39a883f12a075e0271d349e48
AnnamSoumya <36558860+AnnamSoumya@users.noreply.github.com>
2018-02-21 15:33:06 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,10 @@
 
 
-docker run -id centos
+docker run -d centos
+yum install wget -y
+wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.50/bin/apache-tomcat-8.0.50.tar.gz
+mkdir /opt/tomcat   
+tar -zxvf apache-tomcat-8.0.50.tar.gz -C /opt/tomcat
+cd /opt/tomcat/apache-tomcat-8.0.50/bin
+./catalina.sh
+ps -ef|grep tomcat
