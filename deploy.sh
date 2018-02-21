64fa49ee7078b46a267b5af6ed4cef9102619bf7
AnnamSoumya <36558860+AnnamSoumya@users.noreply.github.com>
2018-02-21 16:03:26 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,5 @@ yum install wget -y
 wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.50/bin/apache-tomcat-8.0.50.tar.gz
 mkdir /opt/tomcat   
 tar -zxvf apache-tomcat-8.0.50.tar.gz -C /opt/tomcat
-cd /opt/tomcat/apache-tomcat-8.0.50/bin
-pwd
-./catalina.sh start
-ps -ef|grep tomcat
 
 
