f6bec3d81e6a2d69ff5ec09d60da455a7f2eafcb
AnnamSoumya <36558860+AnnamSoumya@users.noreply.github.com>
2018-02-21 15:55:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.50/bin/apache-tomcat-8.0
 mkdir /opt/tomcat   
 tar -zxvf apache-tomcat-8.0.50.tar.gz -C /opt/tomcat
 cd /opt/tomcat/apache-tomcat-8.0.50/bin
+pwd
 ./catalina.sh start
 ps -ef|grep tomcat
 
