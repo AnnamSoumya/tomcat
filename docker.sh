695e66fcbe98b2e06ad86fb98780a1778bc052dc
AnnamSoumya <36558860+AnnamSoumya@users.noreply.github.com>
2018-02-23 19:31:19 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,7 @@
 #docker run -d -p 80:80 yogeshraheja/yogeshnginx
 #test -t 1 && USE_TTY="-t" 
-docker run -it centos /bin/bash
+docker run -d --name=yogeshraheja centos /bin/bash
+sleep 30
+docker attach yogeshraheja
 yum -y install talk
 exit
