915f1b61e91c4b93b67ea0cd3f01261a9eadc943
AnnamSoumya <36558860+AnnamSoumya@users.noreply.github.com>
2018-02-23 19:04:34 +0530

    Yogesh Docker testing for web server

--- docker.sh
+++ docker.sh
@@ -1,10 +1,1 @@
-
-
-docker run -d centos
-yum install wget -y
-wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.50/bin/apache-tomcat-8.0.50.tar.gz
-mkdir /opt/tomcat   
-tar -zxvf apache-tomcat-8.0.50.tar.gz -C /opt/tomcat
-cd /opt/tomcat/apache-tomcat-8.0.50/bin
-./startup.sh
-ps -ef|grep tomcat
+docker run -d -p 80:80 yogeshraheja/yogeshnginx
