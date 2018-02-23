2adbf5d041ad881bed4253de3d25fc4afb543f1c
AnnamSoumya <36558860+AnnamSoumya@users.noreply.github.com>
2018-02-23 19:37:42 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,8 +1,8 @@
 #docker run -d -p 80:80 yogeshraheja/yogeshnginx
 #test -t 1 && USE_TTY="-t" 
-docker run -itd --name=raheja centos /bin/bash
+docker run -itd --name=rahejayogesh centos /bin/bash
 sleep 30
-docker attach raheja
+docker attach rahejayogesh
 yum -y install talk
 yum -y install httpd
 exit
